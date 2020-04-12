71f6d247b6cf3ed2405d2be7a1fe1d369a53ff2e
erfan golbahari <erfanviva@gmail.com>
2020-04-13 00:50:57 +0430

    Updates

--- precache-manifest.10a0f161d3651f5d58a9d15d6e46fed6.js
+++ precache-manifest.10a0f161d3651f5d58a9d15d6e46fed6.js
@@ -1,62 +1,62 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ec2a523ba2dc978725b9f2727a01dba8",
-    "url": "/index.html"
+    "url": "/Pshirt/index.html"
   },
   {
     "revision": "08a28322ab455ff2e4a1",
-    "url": "/static/css/2.1bd2ab2f.chunk.css"
+    "url": "/Pshirt/static/css/2.1bd2ab2f.chunk.css"
   },
   {
     "revision": "adb56262b95ee6f3e9a9",
-    "url": "/static/css/main.aa1cc1a1.chunk.css"
+    "url": "/Pshirt/static/css/main.aa1cc1a1.chunk.css"
   },
   {
     "revision": "08a28322ab455ff2e4a1",
-    "url": "/static/js/2.27edca48.chunk.js"
+    "url": "/Pshirt/static/js/2.27edca48.chunk.js"
   },
   {
     "revision": "973f511867bc028050003b876b659788",
-    "url": "/static/js/2.27edca48.chunk.js.LICENSE.txt"
+    "url": "/Pshirt/static/js/2.27edca48.chunk.js.LICENSE.txt"
   },
   {
     "revision": "adb56262b95ee6f3e9a9",
-    "url": "/static/js/main.a35dbdde.chunk.js"
+    "url": "/Pshirt/static/js/main.a35dbdde.chunk.js"
   },
   {
     "revision": "ad1adc699005d544495d",
-    "url": "/static/js/runtime-main.c4353bd3.js"
+    "url": "/Pshirt/static/js/runtime-main.c4353bd3.js"
   },
   {
     "revision": "da34e53b4b353abba41c3341fd470500",
-    "url": "/static/media/pshirt.da34e53b.png"
+    "url": "/Pshirt/static/media/pshirt.da34e53b.png"
   },
   {
     "revision": "daf1932f09e3a24bad470842f4c26044",
-    "url": "/static/media/pshirtL.daf1932f.png"
+    "url": "/Pshirt/static/media/pshirtL.daf1932f.png"
   },
   {
     "revision": "b705924e85826149d60da6223f1b993b",
-    "url": "/static/media/random1.b705924e.png"
+    "url": "/Pshirt/static/media/random1.b705924e.png"
   },
   {
     "revision": "aff67269f77af8b60c4a9075947d978a",
-    "url": "/static/media/random10.aff67269.webp"
+    "url": "/Pshirt/static/media/random10.aff67269.webp"
   },
   {
     "revision": "0340adaa733554625e9d50c5c093224e",
-    "url": "/static/media/random4.0340adaa.png"
+    "url": "/Pshirt/static/media/random4.0340adaa.png"
   },
   {
     "revision": "30b150cd489202db131009ac9540cec0",
-    "url": "/static/media/random6.30b150cd.png"
+    "url": "/Pshirt/static/media/random6.30b150cd.png"
   },
   {
     "revision": "c435e553779013e0a3081939134127bd",
-    "url": "/static/media/random8.c435e553.jpg"
+    "url": "/Pshirt/static/media/random8.c435e553.jpg"
   },
   {
     "revision": "e570556b2e88df5274dba377a3731573",
-    "url": "/static/media/random9.e570556b.png"
+    "url": "/Pshirt/static/media/random9.e570556b.png"
   }
 ]);
